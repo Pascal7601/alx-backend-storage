c4308d0e60d7c800e36822835984f6a426cd2efd
Pascal7601 <levispascal63@gmail.com>
2024-08-16 02:39:54 -0700

    nginx logs

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+
+
+def stats(collection):
+    """
+    provide stats of logs collection
+    in nginx
+    """
+    all_logs = collection.count_documents({})
+    print(f'{all_logs} logs')
+    print('Methods:')
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        ncol = collection.count_documents({"method": method})
+        print('\tmethod {}: {}'.format(method, ncol))
+
+if __name__ == '__main__':
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    number_col = stats(collection)
