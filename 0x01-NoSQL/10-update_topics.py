68408c091140c443f8f7106c617271360bc9d967
Pascal7601 <levispascal63@gmail.com>
2024-08-16 00:49:42 -0700

    update a collection

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+update a topic with a certain 
+name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    name: the name that defines the clause
+    on where the update should occur
+    topics: the one thet is updated
+    """
+    updated_topics = mongo_collection.update_one({
+        'name': name}, {'$set': {'topics': topics}})
